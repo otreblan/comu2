8bdcc82fbb75bd05cedc9f9f258a8d21d1169b30
otreblan <otreblain@gmail.com>
2019-07-07 12:02:02 -0500

    Unos cambios al gitdata para gitinfo2

--- .gitdata.sh
+++ .gitdata.sh
@@ -1,14 +1,22 @@
 #/bin/bash
 
-touch algoooooooooo
 # The gitinfo2 hook template
 HOOKTMPLT="/usr/share/texmf-dist/doc/latex/gitinfo2/post-xxx-sample.txt"
+REPOROOT=$(git rev-parse --show-toplevel)
+
+# Check error code of last command
+if (( $? != 0 ))
+then
+	echo "This directory isn't inside a git repository" >&2
+	exit 2
+fi
+
 if [[ -e $HOOKTMPLT ]]
 then
+	cd $REPOROOT
 	if [[ ! -e .git ]]; then
 		mkdir .git
 	fi
-	touch funciona
 	bash $HOOKTMPLT
 else
 	echo "Git hook not found, please check your latex distribution" >&2
